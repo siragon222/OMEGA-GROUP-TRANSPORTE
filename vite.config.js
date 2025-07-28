0534338a868f0d599de4668a7b15841fcf6b8eb4
Ronald <siragon222@gmail.com>
2025-07-28 00:04:33 -0400

    28 7 25

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/OMEGA-GROUP-TRANSPORTE',
   plugins: [react()],
-  base: 'OMEGA-GROUP-TRANSPORTE',
 })
